d7f7cd76a2c9c1097e9913093db2e1fe33e25bc3
Dario Furlan <dario@dariofurlan.com>
2022-09-24 23:23:12 +0200

    dev

--- src/application/Home/HomeView.tsx
+++ src/application/Home/HomeView.tsx
@@ -1,9 +1,11 @@
 import { observer } from "mobx-react-lite";
+import Tech from "./Tech";
 import WhoAmI from "./WhoAmI";
 
 
 export default observer(function HomeView() {
     return <>
         <WhoAmI/>
+        <Tech/>
     </>
 })
