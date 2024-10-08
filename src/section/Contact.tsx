e5996614657d26d25514897c796d95efc74bfcef
Dario Furlan <dario@dariofurlan.com>
2024-10-08 09:06:23 +0200

    oplà

--- src/section/Contact.tsx
+++ src/section/Contact.tsx
@@ -3,38 +3,40 @@
 export default function Contact() {
     return <>
 
-        <div className="mx-auto card bg-base-300 w-full max-w-sm shrink-0 shadow-xl">
+        <div className="mx-auto card bg-base-200 w-full max-w-sm shrink-0 shadow-gray-400 shadow-xl my-8">
             <form className="card-body"
                 action="https://formie.io/form/2845024d-fc14-409d-aaa9-22dcb1e8d0c3"
                 method="POST">
-                <h2 id="contact" className="card-title text-3xl font-Outfit">Contattami</h2>
+                <h2 id="contact" className="card-title text-3xl font-Outfit self-center">Contattami</h2>
 
                 <input name="_redirect" type="hidden" value="https://dariofurlan.com/#form-success" />
 
                 <input name="_redirect_error" type="hidden" value="https://dariofurlan.com/#form-error" />
 
-                <label className="input input-bordered flex items-center gap-2">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 16 16"
-                        fill="currentColor"
-                        className="h-4 w-4 opacity-70">
-                        <path
-                            d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
-                        <path
-                            d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
-                    </svg>
-                    <input name="email" type="email" className="grow" placeholder="Email" />
-                </label>
+                <div className="form-control">
+                    <label htmlFor="name" className="label">
+                        <span className="label-text">Nome</span>
+                    </label>
+                    <input className="input input-bordered grow" name="name" type="text" placeholder="Mario Rossi" required/>
+                </div>
 
-                <textarea
-                    name="query"
-                    placeholder="Bio"
-                    className="textarea textarea-bordered textarea-sm w-full max-w-xs">
+                <div className="form-control">
+                    <label htmlFor="email" className="label">
+                        <span className="label-text">Email</span>
+                    </label>
+                    <input className="input input-bordered grow" name="email" type="email" placeholder="mario.rossi@example.com" required/>
+                </div>
 
-                </textarea>
-
-                <button className="btn btn-primary">Submit</button>
+                <div className="form-control">
+                    <label htmlFor="message" className="label">
+                        <span className="label-text">Messaggio</span>
+                    </label>
+                    <textarea
+                        name="message"
+                        placeholder="Messaggio"
+                        className="textarea textarea-bordered resize-none textarea-sm w-full max-w-xs" required />
+                </div>
+                <button className="mt-4 btn btn-primary">Conferma</button>
             </form>
         </div>
     </>
--- src/section/project/PythIA.tsx
+++ src/section/project/PythIA.tsx
@@ -8,8 +8,8 @@ export default function PythIA() {
             description={<>realizzata come progetto di stage in collaborazione con <a target="_blank" rel="noopener noreferrer" className="link-accent" href="https://sgiservizi.net/">SGI Servizi S.R.L.</a></>}
             icon={<img className="rounded-lg h-16 w-16 p-2 bg-white shadow-xl shadow-gray-400" src="/icons/pythia.svg" alt="PythIA" />}
         />
-        <p className="prose">
-            
+        <p className="prose mx-auto">
+            Durante il mio progetto di stage con l'Università di Padova presso SGI Servizi, ho sviluppato un assistente virtuale per gli operatori di call center di una casa automobilistica. Questo strumento, progettato per supportare le campagne di rifidelizzazione, analizza in tempo reale le conversazioni telefoniche e suggerisce agli operatori i migliori consigli in base all'umore dell'interlocutore. Alla fine di ogni chiamata, l'assistente fornisce un feedback dettagliato sul sentimento dell'utente e un riepilogo dei punti salienti della conversazione, migliorando così l'efficacia delle interazioni e la qualità del servizio offerto.
         </p>
     </ProjectContainer>
 }
