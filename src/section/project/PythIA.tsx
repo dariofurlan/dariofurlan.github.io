dd67d970759cf3c79e37253c852576d084b9ef13
Dario Furlan <dario@dariofurlan.com>
2024-10-10 15:04:56 +0200

    yup

--- src/section/project/PythIA.tsx
+++ src/section/project/PythIA.tsx
@@ -5,7 +5,7 @@ export default function PythIA() {
     return <ProjectContainer>
         <ProjectHead
             title="PythIA"
-            description={<>realizzata come progetto di stage in collaborazione con <a target="_blank" rel="noopener noreferrer" className="link-accent" href="https://sgiservizi.net/">SGI Servizi S.R.L.</a></>}
+            description={<>realizzata in collaborazione con <a target="_blank" rel="noopener noreferrer" className="link-accent" href="https://sgiservizi.net/">SGI Servizi S.R.L.</a> come progetto di stage universitario <a target="_blank" rel="noopener noreferrer" className="link-accent" href="https://unipd.it/">Università di Padova</a></>}
             icon={<img className="rounded-lg h-16 w-16 p-2 bg-white shadow-xl shadow-gray-400" src="/icons/pythia.svg" alt="PythIA" />}
         />
         <p className="prose mx-auto">
